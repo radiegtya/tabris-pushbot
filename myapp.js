867ad6e39ddb6ff32bee3f25a94fcb6fb78febd5
radiegtya <radiegtya@yahoo.co.id>
2015-06-16 21:15:33 +0700

    fresh install

---
+++ myapp.js
@@ -0,0 +1,17 @@
+var page = tabris.create("Page", {
+    title: "Test Pushbot",
+    topLevel: true
+});
+
+//if (PushbotsPlugin.isAndroid()) {
+//    PushbotsPlugin.initializeAndroid("55802c4117795971628b4567", "421100931770");
+//}
+
+var myTextView = tabris.create("TextView", {
+    layoutData: {centerX: 0, centerY: 0},
+    text: "Hello Pushbot"
+}).appendTo(page);
+
+page.open();
+
+      
